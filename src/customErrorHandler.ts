e130c5cb2a5a1aadc432b0eb1fc8f134fc58d1d5
Anup Kumar <anupsharma.1stoct@gmail.com>
2026-01-30 21:58:19 +0530

    changes in custom error file

--- src/customErrorHandler.ts
+++ src/customErrorHandler.ts
@@ -6,25 +6,28 @@ type errOptions = {
 
 class AppError extends Error {
     status: number;
+    code: string;
     constructor(message:string="Internal Error", {status=400, code="INTERNAL_ERROR"}: errOptions={}){
         super(message);
         this.status = status;
+        this.code = code;
     }
 }
 
 class CreateError extends AppError{
-    constructor(message:string="Create Error"){
+    constructor(message:string="Create Error", {status=401, code= "CREATE_ERROR"}:errOptions={}){
         super(message, {status:401, code: "CREATE_ERROR"});
+        this.status = status;
+        this.code = code;
     }
 }
 
 class FetchError extends AppError{
-    constructor(message:string="Fetch Error"){
+    constructor(message:string="Fetch Error", {status=404, code= "FETCH_ERROR"}:errOptions={}){
         super(message, {status:404, code: "FETCH_ERROR"});
+        this.status = status;
+        this.code = code;
     }
 }
-
-
-
 
 export {AppError, CreateError, FetchError};
